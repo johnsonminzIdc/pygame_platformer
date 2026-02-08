b33fab6d46ac11b3ae5a1a6377d8b73eab3c82dc
johnsonminzIdc <11jahn@gmail.com>
2026-02-08 12:05:41 +0530

    Added bullet and impproved collision aabb

--- ctr.py
+++ ctr.py
@@ -7,11 +7,12 @@ class Character:
         self.current_speed_y=0
         self.is_jumping=True
         self.facing=1
+        self.is_on_ground=False
         
         
     def apply_gravity(self):
         self.current_speed_y += settings.GRAVITY
-        self.Rect.y += self.current_speed_y
+        self.Rect.y += int(self.current_speed_y)
         
     def apply_bound(self):
        if self.Rect.left <=0:
@@ -20,22 +21,57 @@ class Character:
            self.Rect.right=settings.WIDTH
        if self .Rect.top<=0:
            self.Rect.top=0
-       if self.Rect.bottom>=settings.HIGHT:
-           self.Rect.bottom=settings.HIGHT
-    
-    def check_collision(self,lst):
+       if self.Rect.bottom>=settings.HEIGHT:
+           self.Rect.bottom=settings.HEIGHT
+
+    def check_collision_x(self, lst):
         for items in lst:
-            check_Rect=pygame.Rect(items)
-            if self.Rect.colliderect(check_Rect):
-                if self.current_speed_y>0:
-                    self.Rect.bottom=check_Rect.top
-                    self.current_speed_y=0
+            check_rect = pygame.Rect(items)
+            if self.Rect.colliderect(check_rect):
+                # Use position relative to the block to decide which way to push
+                if self.Rect.centerx < check_rect.centerx:
+                    self.Rect.right = check_rect.left
+                else:
+                    self.Rect.left = check_rect.right                
+    
+    
+    
+    
+    
+    '''def check_collision_x(self,lst):
+        for items in lst:
+            check_rect=pygame.Rect(items)
+            if self.Rect.inflate(0,-8).colliderect(check_rect.inflate(0,-4)):
+                vertical_gap = check_rect.top - self.Rect.bottom
+
+             # If platform is below us, ignore horizontal collision coyote margin
+                if vertical_gap > -4:
+                    continue
+                if self.facing >0 :
+                    self.Rect.right=check_rect.left    
+                elif self.facing <0 :
+                    self.Rect.left=check_rect.right'''
+    
+                        
+    def check_collision_y(self,lst):
+        
+        for items in lst:
+            check_rect=pygame.Rect(items)
+            if self.Rect.inflate(-4,0).colliderect(check_rect):
+                if self.current_speed_y>0: #falling down              
+                    self.Rect.bottom=check_rect.top
                     self.is_jumping=False
+                    self.is_on_ground=True
+                    self.current_speed_y=0
+                elif self.current_speed_y<0: #jumping up head hit
+                    self.Rect.top=check_rect.bottom
+                    self.current_speed_y=0
     #jump logic can be made by checking if player is ground and pressed spacebar then current_speed_y = jump_speed -gravity
                     
     def jump(self,jump_trigger):
         if jump_trigger ==1 and self.is_jumping == False:
             self.is_jumping=True
+            self.is_on_ground=False
             self.current_speed_y = -settings.JUMP_SPEED
     
     #def can_attack():
@@ -52,19 +88,33 @@ class Character:
             
     
     #attack logic is swaning point in x direction / or in key direction like contra
-    def move(self,dx,dy):
-        self.Rect.x += dx*settings.PLAYER_SPEED_X
-        self.Rect.y += dy*settings.PLAYER_SPEED_Y
+
+
+    def move_x(self,dx):
+        self.Rect.x +=int(dx*settings.PLAYER_SPEED_X)
         if dx>0:
             self.facing=1
         elif dx<0:
             self.facing=-1
+            
+    def move_y(self,dy):
+        self.Rect.y += dy*settings.PLAYER_SPEED_Y
     
-    def update(self,lst):
+    def update(self,lst,dx):
         #self.jump(jump_trigger)
-        self.check_collision(lst)
-        self.apply_bound()
+        self.is_on_ground=False
+        self.move_x(dx)
+        #self.Rect.x += dx*settings.PLAYER_SPEED_X
+        self.check_collision_x(lst) 
+        #self.move_y()
         self.apply_gravity()
+        
+        self.check_collision_y(lst)
+        
+        if self.is_on_ground:
+            self.current_speed_y=0
+        self.apply_bound()
+        
         
     
     def draw(self,surface,color):
